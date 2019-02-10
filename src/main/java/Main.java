f6102b448e2e4331ebb6d0b2c31c485635b951d1
Rob <balonekrobert@gmail.com>
2019-02-10 16:27:35 +0100

    checking ext added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,19 +9,39 @@ public class Main {
 
     }
 
-    public void isFileExtensionIsTrue(File file) throws IOException {
+    public boolean isFileExtensionIsTrue(File file) throws IOException {
         DataInputStream input = new DataInputStream(new BufferedInputStream(new FileInputStream(file)));
         int fileSignature = input.readInt();
         input.close();
-        String ext = FilenameUtils.getExtension(file.getAbsolutePath());
+        String ext = FilenameUtils.getExtension(file.getPath());
 
 
 
-        if(fileSignature == 0xffd8){ ;
+
+        if(fileSignature == 0xffd8){
+            if(ext.equals("jpg") || ext.equals("jpeg")){
+                System.out.println("this is a " + ext + " file");
+                return true;
+            }else {
+                System.out.println("extension is " + ext + " while actual extension is  "+ checkWhatsTheExtension(fileSignature));
+                return false;
+            }
+
 
         }
-        
+
+
+        return false;
 
     }
 
+    private String checkWhatsTheExtension ( int fileSignature){
+        if(fileSignature == 0xffd8){
+            return "jpg or jpeg";
+        }else if(fileSignature ==0){
+            return "txt";
+        }else if(fileSignature == 0x47) {
+            return "gif";
+        }else return "unknown data type";
+    }
 }
