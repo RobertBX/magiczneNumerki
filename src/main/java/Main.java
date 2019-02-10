8e6a408444310e0adec69ae99184334f97524647
Rob <balonekrobert@gmail.com>
2019-02-10 16:07:40 +0100

    checking data type

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,26 @@
-import java.io.File;
+import jdk.internal.instrumentation.Logger;
+import org.apache.commons.io.FilenameUtils;
+
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
+    }
+
+    public void isFileExtensionIsTrue(File file) throws IOException {
+        DataInputStream input = new DataInputStream(new BufferedInputStream(new FileInputStream(file)));
+        int fileSignature = input.readInt();
+        input.close();
+        String ext = FilenameUtils.getExtension(file.getAbsolutePath());
+
+
+
+        if(fileSignature == 0xffd8){ ;
+
+        }
+        
 
     }
 
